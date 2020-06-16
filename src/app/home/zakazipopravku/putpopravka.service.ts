8cf29058425369f234d7d292e06ddacfc186ec3a
alekap <alekap995@gmail.com>
2020-06-16 12:00:08 +0200

    2 files modified

--- src/app/home/zakazipopravku/putpopravka.service.ts
+++ src/app/home/zakazipopravku/putpopravka.service.ts
@@ -6,7 +6,7 @@ export class PutpopravkaService {
 
   constructor(private http: HttpClient) { }
  putPopravku(sasija,rm,opis): Observable<any> {
-    return this.http.post('https://cors-anywhere.herokuapp.com/http://178.221.149.5:8081/popravke',JSON.stringify({
+    return this.http.post('https://cors-anywhere.herokuapp.com/http://178.221.149.5:8081/api/popravke',JSON.stringify({
         broj_sasije: sasija,
         sifra_rm: rm,
         opis: opis
